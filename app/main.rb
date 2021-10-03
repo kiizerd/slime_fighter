80acbff447f14009addc7bcae8fd1a2fa80bb96d
kiizerd <ajsewell13@gmail.com>
2021-10-02 21:03:42 -0500

    required needed files

--- app/main.rb
+++ app/main.rb
@@ -1,6 +1,12 @@
 APP = 'app/'.freeze
 LIB = 'lib/'.freeze
 
+OBJ = 'app/objects/'.freeze
+
+require "#{OBJ}player.rb"
+
+require "#{OBJ}enemy.rb"
+
 require "#{APP}game.rb"
 
 require "#{APP}tick.rb"
