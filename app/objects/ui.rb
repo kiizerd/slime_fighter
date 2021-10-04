a75dfa83c19e1d123fe8541352bbf4d157ab4eb1
kiizerd <ajsewell13@gmail.com>
2021-10-04 17:35:30 -0500

    added title scene ui bits

--- app/objects/ui.rb
+++ app/objects/ui.rb
@@ -1,5 +1,4 @@
 class UI
-
   # TODO: replace all outputs with render_targets
 
   attr_gtk
@@ -9,16 +8,28 @@ class UI
   end
 
   def tick
-    player_health && player_score
+    render_scene_ui
+  end
+
+  def render_scene_ui
+    case state.scene
+    when :title then title_ui
+    when :game  then game_ui
+    end
+  end
+
+  def title_ui
+    title
+  end
+
+  def game_ui
+    # player_health
+    player_score
   end
 
   def player_health
     outputs.borders << health_border
     outputs.solids  << health_bar
-  end
-
-  def health_label
-
   end
 
   def health_border
@@ -62,5 +73,31 @@ class UI
       x: grid.right.half - 70, y: grid.top - 50,
       w: 140, h: 40, r: 250, g: 150, b: 100, a: 150
     }
+  end
+
+  def title
+    outputs.labels << [title_header, title_prompt]
+  end
+
+  def title_header
+    text = 'Slime Fighter'
+    w, h = gtk.calcstringbox text
+    x    = grid.right.half - w.half
+    y    = grid.top - h * 8
+
+    [x, y, text]
+  end
+
+  def title_prompt
+    text = 'Press Space to begin'
+    w, h = gtk.calcstringbox text
+    x    = grid.right.half - w.half
+    y    = grid.top - h * 12
+
+    [x, y, text]
+  end
+
+  def title_splash
+    
   end
 end
